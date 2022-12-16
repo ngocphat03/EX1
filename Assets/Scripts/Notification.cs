76c7883430b27e3effd6a36aa3fc4d0d74fe9ae9
ngocphat03 <107571396+ngocphat03@users.noreply.github.com>
2022-12-16 18:18:25 +0700

    finish

--- Assets/Scripts/Notification.cs
+++ Assets/Scripts/Notification.cs
@@ -1,17 +1,47 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
+using TMPro;
 
 public class Notification : MonoBehaviour
 {
+    public UnityEngine.UI.Image avatar;
+    public TMP_InputField inputName;
+    public TMP_InputField inputRule;
+    public TextMeshProUGUI textScreen;
     public Button btnOk;
+
+    private string changeTextInScreen = "You need to fill in additional fields: ";
 
     private void Start()
     {
         this.btnOk.onClick.AddListener(ClosePopup);
+        avatar = (UnityEngine.UI.Image)GameObject.FindWithTag("Avatar").GetComponent<UnityEngine.UI.Image>();
+        inputName = (TMP_InputField)GameObject.FindWithTag ("InputName").GetComponent<TMP_InputField>();
+        inputRule = (TMP_InputField)GameObject.FindWithTag ("InputRule").GetComponent<TMP_InputField>();
+        ChangeText();
     }
 
     private void ClosePopup()
     {
         Destroy(gameObject);
+    }
+
+    private void ChangeText()
+    {
+        if(avatar.sprite == null)
+        {
+            changeTextInScreen += "choose avatar, ";
+        }
+        if(inputName.text == "")
+        {
+            changeTextInScreen += "name ";
+        }
+        if(inputRule.text == "")
+        {
+            changeTextInScreen += "rule ";
+        }
+        Debug.Log(changeTextInScreen);
+        textScreen.text = changeTextInScreen;
     }
 }
